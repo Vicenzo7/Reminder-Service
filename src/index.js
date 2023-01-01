d5eae9716a2adc33d4c2286152b70dbe4a2482d3
Vicenzo7 <deeprajm35@gmail.com>
2023-01-02 00:39:09 +0530

    Cron every two minutes to send email

--- src/index.js
+++ src/index.js
@@ -3,27 +3,28 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
-const cron = require("node-cron");
+// const { sendBasicEmail } = require("./services/email-service");
+
+const jobs = require("./util/job");
+const TicketController = require("./controllers/ticket-controller");
+
 const setupAndStartServer = () => {
   const app = express();
 
-  app.use(bodyParser.json);
+  app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.post("/api/v1/tickets", TicketController.create);
 
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
-
+    jobs();
     // sendBasicEmail(
     //   "Support from support@admin.com",
     //   "deeprajm35@gmail.com",
     //   "Booking confirmed",
     //   "Hey Your flight journey has been confirmed"
     // );
-
-    cron.schedule("*/2 * * * *", () => {
-      console.log("running a task every two minutes");
-    });
   });
 };
 
--- src/models/notificationticket.js
+++ src/models/notificationticket.js
@@ -15,11 +15,12 @@ module.exports = (sequelize, DataTypes) => {
     {
       subject: { type: DataTypes.STRING, allowNull: false },
       content: { type: DataTypes.STRING, allowNull: false },
-      recipientEmail: { type: ataTypes.STRING, allowNull: false },
+      recipientEmail: { type: DataTypes.STRING, allowNull: false },
       status: {
         type: DataTypes.ENUM,
         allowNull: false,
         values: ["PENDING", "SUCCESS", "FAILED"],
+        defaultValue: "PENDING",
       },
       notificationTime: { type: DataTypes.DATE, allowNull: false },
     },
